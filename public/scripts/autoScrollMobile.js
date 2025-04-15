f9e095608c7486c13e60874bc26a2d3d21d47e53
birando <birger1@live.com>
2025-04-15 10:00:28 +0200

    feat(CSS): add better UX and UI

---
+++ public/scripts/autoScrollMobile.js
@@ -0,0 +1,17 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const isMobile = window.innerWidth < 768; // Mobile threshhold
+
+  if (isMobile) {
+    const inputs = document.querySelectorAll("input, textarea, select");
+
+    inputs.forEach((input) => {
+      input.addEventListener("focus", () => {
+        // Scroll with an offset for sticky header
+        const yOffset = -80;
+        const y = input.getBoundingClientRect().top + window.scrollY + yOffset;
+
+        window.scrollTo({ top: y, behavior: "smooth" });
+      });
+    });
+  }
+});
